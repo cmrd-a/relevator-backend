f79474f7994f46ae77f1556242e33427361fb23d
cmrd-a <cmrd.a@ya.ru>
2021-04-27 22:20:51 +0300

    Added initial files

--- app/main.py
+++ app/main.py
@@ -1,13 +1,48 @@
-from typing import Optional
-from fastapi import FastAPI
+import uvicorn
+from fastapi import Depends, FastAPI, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+
+from app import crud, deps, models, schemas, security
+from app.api import items, users
+from app.database import SessionLocal, engine
+from app.settings import settings
 
 app = FastAPI()
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+app.include_router(users.router, tags=["users"])
+app.include_router(items.router, tags=["items"])
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.on_event("startup")
+def startup_event():
+    models.Base.metadata.create_all(bind=engine)
+    db: Session = SessionLocal()
+    user = crud.get_user_by_email(db, settings.super_user_email)
+    if not user:
+        user_in = schemas.UserCreate(
+            email=settings.super_user_email, password=settings.super_user_password
+        )
+        crud.create_user(db, user_in)
+    db.close()
+
+
+@app.post("/login/access-token", response_model=schemas.Token)
+def login_access_token(
+        db: Session = Depends(deps.get_db),
+        form_data: OAuth2PasswordRequestForm = Depends()
+):
+    """OAuth2 compatible token login, get an access token for future requests."""
+    user = crud.authenticate_user(
+        db, email=form_data.username, password=form_data.password
+    )
+    if not user:
+        raise HTTPException(status_code=400, detail="Incorrect email or password")
+    return {
+        "access_token": security.create_access_token(subject=user.id),
+        "token_type": "bearer",
+    }
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
